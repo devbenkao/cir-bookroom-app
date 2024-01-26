6f678b810921be667c9548c198d86d784f46961c
Ben Kao <benkao@Ben-Kaos-MacBook-Pro.local>
2024-01-26 11:16:29 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Church In Richardson Book Room</h1>
+
+      <button>Check out a new book</button>
+      <button>Return a book</button>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HomePage />
     </div>
   );
 }
